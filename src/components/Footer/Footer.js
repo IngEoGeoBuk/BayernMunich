84b132c860dc980316eae1bbc499bd37dfd437d3
IngEoGeoBuk <59249899+IngEoGeoBuk@users.noreply.github.com>
2020-11-14 16:37:42 +0900

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -20,8 +20,12 @@ function Footer() {
         }
     `
 
-    const FooterImg = {
+    const FooterImgPcVer = {
         padding: "30px"
+    }
+
+    const FooterImgMobileVer = {
+        padding: "10px 20px"
     }
 
     return (
@@ -30,35 +34,36 @@ function Footer() {
                 <PcVer>
                     <img src = "https://i.esdrop.com/d/8gqkv7Kpzr.png" width="10%"/>
                 </PcVer>
-                <MobileVer style={{ paddingTop:"40px" }}>
-                    <img src = "https://i.esdrop.com/d/8gqkv7Kpzr.png" width="20%"/>
+                <MobileVer>
+                    <img src = "https://i.esdrop.com/d/8gqkv7Kpzr.png" width="90em"/>
                 </MobileVer>
             </div>
             <div style = {{ backgroundColor: 'rgba(220, 5, 44)', color: "white"}}>
                 <br></br><br></br>
                 <Typography variant="h6">  
                     PARTNER
+                    <br></br><br></br>
                 </Typography>
 
                 <PcVer>
                     <Box display="flex" flexDirection="row" justifyContent= "center" p={0} m={0}>
                         <Box p={0}>
-                            <div style={FooterImg}>
+                            <div style={FooterImgPcVer}>
                                 <a href="/BayernMunich"><img src = "https://i.esdrop.com/d/B5Qe3xFsyM.png" width="100px"/></a>
                             </div>
                         </Box>
                         <Box p={0}>
-                            <div style={FooterImg}>
+                            <div style={FooterImgPcVer}>
                                 <a href="/BayernMunich"><img src = "https://i.esdrop.com/d/6yxZfr65jA.png" width="100px"/></a>
                             </div>
                         </Box>
                         <Box p={0}>
-                            <div style={FooterImg}> 
+                            <div style={FooterImgPcVer}> 
                                 <a href="/BayernMunich"><img src = "https://i.esdrop.com/d/1W02FKOiHp.png" width="100px"/></a>
                             </div>
                         </Box>
                         <Box p={0}>
-                            <div style={FooterImg}>
+                            <div style={FooterImgPcVer}>
                                 <a href="/BayernMunich"><img src = "https://i.esdrop.com/d/hg6NTUaDW7.png" width="100px"
                                     style={{paddingTop:"20px"}}
                                 /></a>
@@ -71,23 +76,20 @@ function Footer() {
                     <Grid container spacing={0}>
                         <Grid item xs={12}>
                             <Box display="flex" flexDirection="row" p={0} m="auto" justifyContent= "center">
-                                <div style={FooterImg}>
+                                <div style={FooterImgMobileVer}>
                                     <a href="/BayernMunich"><img src = "https://i.esdrop.com/d/B5Qe3xFsyM.png" width="100px"/></a>
                                 </div>
-                                <div style={FooterImg}>
+                                <div style={FooterImgMobileVer}>
                                     <a href="/BayernMunich"><img src = "https://i.esdrop.com/d/6yxZfr65jA.png" width="100px"/></a>
                                 </div>
-                                
-                                
                             </Box>
                         </Grid>
-
                         <Grid item xs={12}>
                             <Box display="flex" flexDirection="row" p={0} m="auto" justifyContent= "center">
-                                <div style={FooterImg}> 
+                                <div style={FooterImgMobileVer}> 
                                     <a href="/BayernMunich"><img src = "https://i.esdrop.com/d/1W02FKOiHp.png" width="100px"/></a>
                                 </div>
-                                <div style={FooterImg}>
+                                <div style={FooterImgMobileVer}>
                                     <a href="/BayernMunich"><img src = "https://i.esdrop.com/d/hg6NTUaDW7.png" width="100px"
                                         style={{paddingTop:"20px"}}
                                     /></a>
